9537bc54ea1b8069c00f3c3d7ba4f013d668ed30
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-04-13 20:45:44 -0300

    tareas agregadas

---
+++ tareas.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const tareasJson = JSON.parse(fs.readFileSync(__dirname + '/notas.json', 'utf-8'))
+let yargs = require('yargs')
+let comando = yargs.argv.comando //lee por consola --comando y asigna como valor lo que sigue
+let titulo = yargs.argv.titulo //lee por consola --titulo y asigna como valor lo que sigue
+let estado = yargs.argv.estado//lee por consola --estado y asigna como valor lo que sigue
+
+
+//agrgando como parametro el __dirname + la ruta de archivo json) devuelve 
+function leerJSON(ruta) {
+    return JSON.parse(fs.readFileSync(ruta, 'utf-8'))
+
+}
+//utilizando la funcion anterior y la ruta , va a mostrar la lista de notas 
+function listar(ruta) {
+    leerJSON(ruta).notas.forEach(function (elementos, i) {
+        console.log(`--- Nota: ${i + 1} ---
+            tarea: ${elementos.titulo}
+            estado: ${elementos.estado}`)
+    })
+}
+//recibe un array de tareas y lo escribe sobre el archivo json que ya tengo 
+function escribirJSON(array) {
+    return fs.writeFileSync(__dirname + '/notas.json', JSON.stringify(array))
+
+
+}
+//guarda en el json de tareas el nuevo objeto
+function guardarTarea(objeto) {
+    tareasJson.notas.push(objeto);
+    escribirJSON(tareasJson)
+}
+//filtra elementos por su estado 
+function leerPorEstado(estado) {
+    return tareasJson.notas.filter(function (e) {
+        return e.estado === estado
+
+    })
+
+
+}
+
+module.exports = {
+    listar,
+    escribirJSON,
+    guardarTarea,
+    leerPorEstado,
+}
